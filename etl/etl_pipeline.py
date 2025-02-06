6597395850380a2902b30967c9c622dc0e342316
danilomassoni <massonidanilo@gmail.com>
2025-02-05 21:38:06 -0300

    Criado o módulo de load e o pipeline para realizar a orquestração

--- etl/etl_pipeline.py
+++ etl/etl_pipeline.py
@@ -1,1 +1,35 @@
 # Script principal que executa o ETL completo
+import os
+from etl.extract import extract_data
+from etl.transform import transform_data
+from etl.load import load_to_postgres
+
+def run_etl():
+    """
+    Orquestração do pipeline ETL: Extração, transformação e Carga. 
+    """
+
+    # Configuração
+    raw_data_path = "data/raw/merged_df.csv"
+    processed_data_path = "data/processed/transformed_data.csv"
+    indicadores = "indicadores_educacionais"
+    db_url = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/meubanco")
+
+    # Extração
+    print ("Extraindo os dados........")
+    df = extract_data(raw_data_path)
+
+    # Transformação
+    print("Transformando os dados.........")
+    df = transform_data(df)
+    df.to_csv(processed_data_path, index=False)
+
+    # Carga
+    print("Carregando os dados no banco............")
+    load_to_postgres(df, indicadores, db_url)
+
+    print("PIPELINE ETL CONCLUÍDO COM SUCESSO!")
+
+if __name__ == "__main__":
+    run_etl()
+    
--- etl/load.py
+++ etl/load.py
@@ -1,1 +1,25 @@
 # Código para carregar os dados no banco de dados
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+
+def load_to_postgres(df: pd.DataFrame, indicadores: str, db_url: str):
+    """
+    Carrega os dados transformados para um banco de dados PostgreSQL
+
+    :param df: DataFram contendo os dados processados
+    :param indicadores: Tabela onde os dados irão ser inseridos
+    :param db_url: URL de conexão com o PostgreSQL
+    """
+    engine = create_engine(db_url)
+
+    with engine.connect() as connection:
+        df.to_sql(indicadores, con=connection, if_exists='replace', index=False)
+
+        print(f"Dados carregados com sucesso na tablea '{indicadores}'!")
+
+# Teste de Carga 
+if __name__ == "__main__":
+    DATABASE_URL =os.getenv("DATABASE_URL", "postgresql://postgres:1234d@localhost:5432/educacao")
+    df = pd.read_csv("./data/processed/trasnformed_data.csv")
+    load_to_postgres(df, "indicadores_educacionais", DATABASE_URL)
