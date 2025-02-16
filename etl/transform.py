2b6039ec8d3b9d1b7a7f21c7c2121208bda99072
danilomassoni <massonidanilo@gmail.com>
2025-02-16 16:24:06 -0300

    Criando a função para adicionar a média por região para a coluna de Remuneração

--- etl/transform.py
+++ etl/transform.py
@@ -38,6 +38,42 @@ def add_country(df: pd.DataFrame) -> pd.DataFrame:
     df["country"] = "Brasil"
     return df
 
+def add_media_region(df: pd.DataFrame, regiao_col, remuneracao_col) -> pd.DataFrame:
+    """
+    Calcula a média de remuneração por região
+    """
+    # Dicionário para mapear estados para regiões
+    estado_para_regiao = {
+    "AC": "Norte", "AM": "Norte", "AP": "Norte", "PA": "Norte", "RO": "Norte", "RR": "Norte", "TO": "Norte",
+    "AL": "Nordeste", "BA": "Nordeste", "CE": "Nordeste", "MA": "Nordeste", "PB": "Nordeste", "PE": "Nordeste",
+    "PI": "Nordeste", "RN": "Nordeste", "SE": "Nordeste",
+    "ES": "Sudeste", "MG": "Sudeste", "RJ": "Sudeste", "SP": "Sudeste",
+    "PR": "Sul", "RS": "Sul", "SC": "Sul",
+    "DF": "Centro-Oeste", "GO": "Centro-Oeste", "MT": "Centro-Oeste", "MS": "Centro-Oeste"
+    }
+
+    # Iterar por cada região única no DataFrama 
+    for regiao in df[regiao_col].unique():
+        if regiao in estado_para_regiao.values():
+            # Filtrar os estados que pertencem à região atual
+            estados_da_regiao = [estado for estado, reg in estado_para_regiao.items() if reg == regiao]
+            
+            # Filtrar os dados dos estados que pertencem à região
+            estados_df = df[df[regiao_col].isin(estados_da_regiao)]
+
+            # Calcular a média da remuneração para os estados da região
+            media_remuneracao = estados_df[remuneracao_col].mean()
+
+            # Preencher a linha da região com a média calculada
+            df.loc[df[regiao_col] == regiao, remuneracao_col] = media_remuneracao
+
+    return df
+
+
+
+
+
+
 def transform_data(df: pd.DataFrame) -> pd.DataFrame:
     """
     Aplica todas as transformações necessárias
@@ -46,6 +82,7 @@ def transform_data(df: pd.DataFrame) -> pd.DataFrame:
     df = handle_missing_values(df)
     df = convert_data_types(df)
     df = add_country(df)
+    df = add_media_region(df, "unidade_geográfica", "remuneração_média")
 
     df.to_csv("./data/processed/transformed_df.csv", index=False) # Cria um arquivo CSV com os dados transformados
     print("Dados transformados e salvo com sucesso!")
--- src/model/data_modeling.py
+++ src/model/data_modeling.py
@@ -1,2 +1,12 @@
 # Criação de tabelas fato/dimensão
+import pandas as pd
+from database.connection import get_connection
 
+def creat_fact_dim_tables():
+    """
+    Cria tabelas fato e dimensão para modelagem de dados no Power BI.
+    """
+
+    conn = get_connection()
+
+    
